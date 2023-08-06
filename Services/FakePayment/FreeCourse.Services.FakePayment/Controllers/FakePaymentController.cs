95737a2744311b582a71908682380811043e5635
Doğukan Canerler <dogukanc760@hotmail.com>
2023-08-06 22:21:19 +0300

    Fake payment, Gateway, RabbitMQ, MassTransit, Ocelot has been added

---
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentController.cs
@@ -0,0 +1,55 @@
+﻿using FreeCourse.Services.FakePayment.Models;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Dtos;
+using FreeCourse.Shared.Messages;
+
+using MassTransit;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentController : CustomBaseController
+    {
+        private readonly ISendEndpointProvider _sendEndpointProvider;
+
+        public FakePaymentController(ISendEndpointProvider sendEndpointProvider)
+        {
+            _sendEndpointProvider = sendEndpointProvider;
+        }
+
+        public async Task<IActionResult> RecievePayment(PaymentDto paymentDto)
+        {
+            var sendEndpoint = await _sendEndpointProvider.GetSendEndpoint(new System.Uri("queue:create-order-service"));
+
+            var createOrderMessageCommand = new CreateOrderMessageCommand();
+            createOrderMessageCommand.BuyerId = paymentDto.Order.BuyerId;
+            createOrderMessageCommand.Province = paymentDto.Order.Address.Province;
+            createOrderMessageCommand.District = paymentDto.Order.Address.District;
+            createOrderMessageCommand.Street = paymentDto.Order.Address.Street;
+            createOrderMessageCommand.Line = paymentDto.Order.Address.Line;
+
+            paymentDto.Order.OrderItems.ForEach(x =>
+            {
+                createOrderMessageCommand.OrderItems.Add(new OrderItem
+                {
+                    PictureUrl = x.PictureUrl,
+                    Price = x.Price,
+                    ProductId = x.ProductId,
+                    ProductName = x.ProductName
+                });
+            });
+
+
+            await sendEndpoint.Send<CreateOrderMessageCommand>(createOrderMessageCommand);
+
+
+            return CreateActionResultInstance<NoContent>(ResponseDto<NoContent>.Success(204));
+        }
+    }
+}
--- Services/Order/FreeCourse.Services.Order.API/Startup.cs
+++ Services/Order/FreeCourse.Services.Order.API/Startup.cs
@@ -1,5 +1,8 @@
+using FreeCourse.Services.Order.Application.Consumer;
 using FreeCourse.Services.Order.Infrastructure;
 using FreeCourse.Shared.Services;
+
+using MassTransit;
 
 using MediatR;
 
@@ -36,6 +39,27 @@ namespace FreeCourse.Services.Order.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumer<CreateOrderMessageCommandConsumer>();
+
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(Configuration["RabbitMQUrl"], "/", host =>
+                    {
+                        host.Username("guest");
+                        host.Password("guest");
+                    });
+
+                    cfg.ReceiveEndpoint("create-order-service", e =>
+                    {
+                        e.ConfigureConsumer<CreateOrderMessageCommandConsumer>(context);
+                    });
+                });
+            });
+
+            services.AddMassTransitHostedService();
             var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
